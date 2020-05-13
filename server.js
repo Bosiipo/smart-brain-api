252abfe5f8b0b4b83c2507508d15c7ed6673446e
Bosiipo <olabosipo07@gmail.com>
2020-05-13 10:22:51 +0100

    changing response

--- server.js
+++ server.js
@@ -25,10 +25,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send(database.users);
-});
-
+app.get('/', (req, res) => { res.send('it is working!')});
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
